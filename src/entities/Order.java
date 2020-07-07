24f25b23581b717e00635f683361383f316e2985
aline <aline.soares.2@hotmail.com>
2020-07-07 20:05:00 -0300

    Criando construtor para a classe Order

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -8,8 +8,47 @@ import java.util.List;
 import entities.enums.OrderStatus;
 
 public class Order {
+	
 	private Date date = new Date();
 	private OrderStatus status;
 	private Client client;
 	private List<OrderItem> itens = new ArrayList<>();
+	
+	public Order() {
+		
+	}
+	public Order(Date date, OrderStatus status, Client client, List<OrderItem> itens) {
+		this.date = date;
+		this.status = status;
+		this.client = client;
+		this.itens = itens;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+	public List<OrderItem> getItens() {
+		return itens;
+	}
+	
 }
