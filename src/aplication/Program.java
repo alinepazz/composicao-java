cb8cbe9f08131e6b2777766f9278f06fcdb2baa0
aline <aline.soares.2@hotmail.com>
2020-07-09 19:23:20 -0300

    Criando a area Program

---
+++ src/aplication/Program.java
@@ -0,0 +1,57 @@
+package aplication;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.OrderItem;
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter cliente data:");
+		System.out.println("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birth = sdf.parse(sc.next());
+		
+		Client client = new Client(name, email, birth);
+		
+		System.out.println("Enter order data:");
+		System.out.print("Status: ");
+		String status = sc.nextLine();
+		System.out.print("How many items to this order? ");
+		int intem = sc.nextInt();
+		
+		for(int i=1; i<=intem; i++) {
+			System.out.println("Enter "+ "#"+i + "item data:");
+			System.out.print("Product name: ");
+			String nameProduto = sc.nextLine();
+			System.out.print("Product price: ");
+			double price = sc.nextDouble();
+			System.out.print("Quantity: ");
+			int quantidade = sc.nextInt();
+			
+			OrderItem orderItem = new OrderItem(quantidade, price, new Product(nameProduto, price));
+			
+		}
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
